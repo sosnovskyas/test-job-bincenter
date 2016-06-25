9371c50c5ef17a63ba9a1a7fc196485c5436c80e
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-25 23:02:21 +0300

    created component uploader

---
+++ src/components/uploader/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from "react";
+
+export default class Uploader extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return <div>
+      uploader
+    </div>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 'use strict';
 import React from "react";
 import {render} from "react-dom";
+
+import Uploader from './components/uploader'
+
+render(
+  <Uploader/>,
+  document.querySelector('.root')
+);
